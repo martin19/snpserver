239ffc429aee522ecf49a4a00cddf57fe81f5ca2
Martin Braun <martinbraun@gmx.net>
2024-10-19 02:22:17 +0200

    working on intel encoder. #3

--- src/stream/video/SnpEncoderVaH264.cpp
+++ src/stream/video/SnpEncoderVaH264.cpp
@@ -8,7 +8,10 @@
 #include "va/va_enc_h264.h"
 #include "va/va_win32.h"
 #include "d3d12.h"
+#include <d3d12video.h>
+#include <dxgi1_4.h>
 
+//TODO: query d3d12 capabilities: (see https://github.com/microsoft/DirectX-Graphics-Samples/blob/master/Samples/Desktop/D3D12HelloWorld/src/HelloVAEncode/D3D12HelloVAEncode.cpp)
 // https://www.vcodex.com/h264avc-picture-management/
 
 #define CHECK_VASTATUS(va_status,func)                                  \
@@ -59,7 +62,52 @@ void SnpEncoderVaH264::stop() {
 }
 
 bool SnpEncoderVaH264::initVaEncoder() {
-   initVaPipeline();
+    initD3D12Pipeline();
+    initVaPipeline();
+}
+
+bool SnpEncoderVaH264::initD3D12Pipeline() {
+    bool result = true;
+    HRESULT status;
+    UINT dxgiFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    // Enable the debug layer (requires the Graphics Tools "optional feature").
+    // NOTE: Enabling the debug layer after device creation will invalidate the active device.
+    {
+        ComPtr<ID3D12Debug> debugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+        {
+            debugController->EnableDebugLayer();
+
+            // Enable additional debug layers.
+            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+        }
+    }
+#endif
+
+    Microsoft::WRL::ComPtr<IDXGIFactory4> factory;
+    status = CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory));
+    CHECK_VASTATUS(status, "CreateDXGIFactory2");
+
+
+    if (useWarpDevice) {
+        status = factory->EnumWarpAdapter(IID_PPV_ARGS(&adapter))
+        CHECK_VASTATUS(status, "factory->EnumWarpAdapter");
+        status = D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0,IID_PPV_ARGS(&device));
+        CHECK_VASTATUS(status, "D3D12CreateDevice");
+    } else {
+        GetHardwareAdapter(factory.Get(), &adapter, true);
+        status = D3D12CreateDevice(adapter.Get(),D3D_FEATURE_LEVEL_11_0,IID_PPV_ARGS(&device));
+        CHECK_VASTATUS(status, "D3D12CreateDevice");
+    }
+
+    // Describe and create the command queue.
+    //TODO below and above GetHardwareAdapter is part of DXSample.h
+
+    return result;
+error:
+    return result;
 }
 
 bool SnpEncoderVaH264::initVaPipeline() {
@@ -72,7 +120,12 @@ bool SnpEncoderVaH264::initVaPipeline() {
     CHECK_RESULT(result, "ensureVaEncSupport");
     result = createVaSurfaces();
     CHECK_RESULT(result, "createVaSurfaces");
-
+    result = importRenderTargetsToVa();
+    CHECK_RESULT(result, "importRenderTargetsToVa");
+    result = initVaProcContext();
+    CHECK_RESULT(result, "initVaProcContext");
+    result = initVaEncContext();
+    CHECK_RESULT(result, "initVaEncContext")
 
     return result;
 error:
@@ -103,6 +156,9 @@ bool SnpEncoderVaH264::ensureVaProcSupport() {
     VAStatus vaStatus;
 
     bool supportsVideoProcessing = false;
+    Microsoft::WRL::ComPtr<ID3D12VideoDevice> spVideoDevice;
+    D3D12_FEATURE_DATA_VIDEO_PROCESS_MAX_INPUT_STREAMS vpMaxInputStreams = {};
+    D3D12_FEATURE_DATA_VIDEO_PROCESS_SUPPORT dx12ProcCaps = {};
 
     int numEntrypoints = vaMaxNumEntrypoints(vaDisplay);
     std::vector<VAEntrypoint> entrypoints(numEntrypoints);
@@ -119,9 +175,41 @@ bool SnpEncoderVaH264::ensureVaProcSupport() {
         return false;
     }
 
-    //TODO: query d3d12 capabilities: (see https://github.com/microsoft/DirectX-Graphics-Samples/blob/master/Samples/Desktop/D3D12HelloWorld/src/HelloVAEncode/D3D12HelloVAEncode.cpp)
+    vaStatus = device->QueryInterface(IID_PPV_ARGS(spVideoDevice.GetAddressOf()));
+    CHECK_VASTATUS(vaStatus, "device->QueryInterface");
+    spVideoDevice->CheckFeatureSupport(D3D12_FEATURE_VIDEO_PROCESS_MAX_INPUT_STREAMS, &vpMaxInputStreams, sizeof(vpMaxInputStreams));
+    numVPRegions = std::min(vpMaxInputStreams.MaxInputStreams, (UINT)4);
+
     // Check VPBlit support for format DXGI_FORMAT_R8G8B8A8_UNORM
+    dx12ProcCaps =
+    {
+        0, // NodeIndex
+        { width, height, { DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709}},
+        D3D12_VIDEO_FIELD_TYPE_NONE,
+        D3D12_VIDEO_FRAME_STEREO_FORMAT_NONE,
+        { 30, 1 },
+        { DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709 },
+        D3D12_VIDEO_FRAME_STEREO_FORMAT_NONE,
+        { 30, 1 },
+    };
+
+    vaStatus = spVideoDevice->CheckFeatureSupport(D3D12_FEATURE_VIDEO_PROCESS_SUPPORT, &dx12ProcCaps, sizeof(dx12ProcCaps));
+    CHECK_VASTATUS(vaStatus, "spVideoDevice->CheckFeatureSupport");
+    if ((dx12ProcCaps.SupportFlags & D3D12_VIDEO_PROCESS_SUPPORT_FLAG_SUPPORTED) == 0) {
+        LOG_F(ERROR, "VAEntrypointVideoProc not supported for format DXGI_FORMAT_R8G8B8A8_UNORM.");
+        return false;
+    }
+
     // Check VPBlit support for format DXGI_FORMAT_R8G8B8A8_UNORM -> DXGI_FORMAT_R8G8B8A8_NV12
+    dx12ProcCaps.OutputFormat.Format = DXGI_FORMAT_NV12;
+    dx12ProcCaps.OutputFormat.ColorSpace = DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P709;
+    vaStatus = spVideoDevice->CheckFeatureSupport(D3D12_FEATURE_VIDEO_PROCESS_SUPPORT, &dx12ProcCaps, sizeof(dx12ProcCaps));
+    CHECK_VASTATUS(vaStatus, "spVideoDevice->CheckFeatureSupport");
+
+    if ((dx12ProcCaps.SupportFlags & D3D12_VIDEO_PROCESS_SUPPORT_FLAG_SUPPORTED) == 0) {
+        LOG_F(ERROR, "VAEntrypointVideoProc not supported for conversion DXGI_FORMAT_R8G8B8A8_UNORM to DXGI_FORMAT_NV12.");
+        return false;
+    }
 
     return result;
 error:
@@ -230,21 +318,138 @@ bool SnpEncoderVaH264::importRenderTargetsToVa() {
         },
     };
 
-    //TODO: uses directx12 api.. either include the api or try working without it.
-    // The value here is an array of num_surfaces pointers to HANDLE so
-    // each handle can be associated with the corresponding output surface
-    // in the call to vaCreateSurfaces
-//    HANDLE renderTargets[FrameCount];
-//    for (size_t i = 0; i < FrameCount; i++)
-//    {
-//        HRESULT hr = m_device->CreateSharedHandle(m_renderTargets[i].Get(),
-//                                                  nullptr,
-//                                                  GENERIC_ALL,
-//                                                  nullptr,
-//                                                  &renderTargets[i]);
-//        ThrowIfFailed(hr);
-//    }
-//    createSurfacesAttribList[2].value.value.p = renderTargets;
+    HANDLE renderTargets[FrameCount];
+    for (size_t i = 0; i < FrameCount; i++) {
+        HRESULT hr = device->CreateSharedHandle(renderTargets[i].Get(), nullptr,GENERIC_ALL,
+                                                  nullptr,&renderTargets[i]);
+        CHECK_VASTATUS(hr, "device->CreateSharedHandle");
+    }
+    createSurfacesAttribList[2].value.value.p = renderTargets;
+
+    // Creates VASurface objects by importing
+    // handles of existing D3D12 resources
+    vaStatus = vaCreateSurfaces(
+            vaDisplay,
+            VA_RT_FORMAT_RGB32,
+            width,
+            height,
+            vaRenderTargets,
+            FrameCount,
+            createSurfacesAttribList,
+            _countof(createSurfacesAttribList));
+    CHECK_VASTATUS(vaStatus, "vaCreateSurfaces");
+
+    return result;
+error:
+    return result;
+}
+
+bool SnpEncoderVaH264::initVaProcContext() {
+    bool result = true;
+    VAStatus vaStatus;
+
+    vaStatus = vaCreateConfig(vaDisplay, VAProfileNone, VAEntrypointVideoProc, nullptr,
+            0,&vaProcConfigId);
+    CHECK_VASTATUS(vaStatus, "vaCreateConfig");
+
+    // Context for color rgb to yuv conversion
+    {
+        vaStatus = vaCreateContext(vaDisplay,vaProcConfigId,(int)width,(int)height,
+                VA_PROGRESSIVE,vaRenderTargets,FrameCount,&vaColorConvCtx);
+        CHECK_VASTATUS(vaStatus, "vaCreateContext");
+
+        vaStatus = vaCreateBuffer(vaDisplay,vaColorConvCtx,VAProcPipelineParameterBufferType,
+                sizeof(VAProcPipelineParameterBuffer),1,nullptr,&vaColorConvBuf);
+        CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
+    }
+
+    // Context for single RGB -> RGB copy
+    {
+        vaStatus = vaCreateContext(vaDisplay,vaProcConfigId,(int)width,(int)height,
+                VA_PROGRESSIVE,vaRenderTargets,FrameCount,&vaCopyCtx);
+        CHECK_VASTATUS(vaStatus, "vaCreateContext");
+
+        vaStatus = vaCreateBuffer(vaDisplay,vaCopyCtx,VAProcPipelineParameterBufferType,
+                sizeof(VAProcPipelineParameterBuffer),1,nullptr,&vaCopyBuf);
+        CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
+    }
+
+    // Context for multiple RGB -> RGB blend
+    {
+        vaStatus = vaCreateContext(vaDisplay,vaProcConfigId,(int)width,(int)height,
+                VA_PROGRESSIVE,vaRenderTargets,FrameCount,&vaBlendCtx);
+        CHECK_VASTATUS(vaStatus, "vaCreateContext");
+
+        vaStatus = vaCreateBuffer(vaDisplay,vaBlendCtx,VAProcPipelineParameterBufferType,
+                sizeof(VAProcPipelineParameterBuffer),1,nullptr,&vaBlendBuf);
+        CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
+
+        vaStatus = vaQueryVideoProcPipelineCaps(vaDisplay,vaBlendCtx,nullptr,0,
+                &procPipelineCaps);
+        CHECK_VASTATUS(vaStatus, "vaQueryVideoProcPipelineCaps");
+
+        int32_t XIncrement = (int32_t)width / numVPRegions;
+        int32_t YIncrement = (int32_t)height / numVPRegions;
+        int32_t XShift = 6;
+        int32_t YShift = 6;
+        for (INT i = 0; i < regionVariations; i++) {
+            pBlendRegions[i][numVPRegions - 1].x = (int16_t)std::max(0, XIncrement - i);
+            pBlendRegions[i][numVPRegions - 1].y = (int16_t)std::max(0, YIncrement - i);
+            pBlendRegions[i][numVPRegions - 1].width = width / 2;
+            pBlendRegions[i][numVPRegions - 1].height = height / 2;
+
+            for (INT j = 0; j < numVPRegions - 1; j++) {
+                pBlendRegions[i][j].x = std::min((int16_t)width, (int16_t)(j * XIncrement + i * 0.25f * j * XShift));
+                pBlendRegions[i][j].y = std::min((int16_t)height, (int16_t)(j * YIncrement + i * 0.5f * YShift));
+                pBlendRegions[i][j].width = regionsSizeRatio * XIncrement;
+                pBlendRegions[i][j].height = regionsSizeRatio * YIncrement;
+            }
+
+            colors[i][0] = 0.0f;
+            colors[i][1] = 0.2f;
+            colors[i][2] = 0.1f + std::min(0.4f, i*0.0125f);
+            colors[i][3] = 1.0f;
+        }
+    }
+
+    return result;
+error:
+    return result;
+}
+
+bool SnpEncoderVaH264::initVaEncContext() {
+    bool result = true;
+    VAStatus vaStatus;
+
+    finalEncodedBitstream.open("output_bitstream.h264", std::ios::binary);
+    vaStatus = vaCreateConfig(vaDisplay,VAProfileH264Main,VAEntrypointEncSlice,
+            nullptr,0,&vaEncConfigId);
+    CHECK_VASTATUS(vaStatus, "vaCreateConfig");
+
+    vaStatus = vaCreateContext(vaDisplay,vaEncConfigId,(int)width,(int)height,
+            VA_PROGRESSIVE,vaRenderTargets,FrameCount,&vaEncContextId);
+    CHECK_VASTATUS(vaStatus, "vaCreateContext");
+
+    vaStatus = vaCreateBuffer(vaDisplay,vaEncContextId,VAEncSequenceParameterBufferType,
+            sizeof(VAEncSequenceParameterBufferH264),1, nullptr,
+            &vaEncPipelineBufferId[VA_H264ENC_BUFFER_INDEX_SEQ]);
+    CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
+
+    vaStatus = vaCreateBuffer(vaDisplay,vaEncContextId,VAEncPictureParameterBufferType,
+            sizeof(VAEncPictureParameterBufferH264),1,nullptr,
+            &vaEncPipelineBufferId[VA_H264ENC_BUFFER_INDEX_PIC]);
+    CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
+
+    vaStatus = vaCreateBuffer(vaDisplay,vaEncContextId, VAEncSliceParameterBufferType,
+            sizeof(VAEncSliceParameterBufferH264),1,nullptr,
+            &vaEncPipelineBufferId[VA_H264ENC_BUFFER_INDEX_SLICE]);
+    CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
+
+    // Worst case within reason assume same as uncompressed surface
+    vaStatus = vaCreateBuffer(vaDisplay,vaEncContextId,VAEncCodedBufferType,
+            width * height * 3,1,nullptr,
+            &vaEncPipelineBufferId[VA_H264ENC_BUFFER_INDEX_COMPRESSED_BIT]);
+    CHECK_VASTATUS(vaStatus, "vaCreateBuffer");
 
     return result;
 error:
--- src/stream/video/SnpEncoderVaH264.h
+++ src/stream/video/SnpEncoderVaH264.h
@@ -4,8 +4,13 @@
 #include <cstdint>
 #include <stream/video/h264/VaBitstream.h>
 #include <d3d12.h>
+#include <wrl.h>
+#include <fstream>
+#include <dxgi1_4.h>
 #include "stream/SnpComponent.h"
 #include "va/va.h"
+
+using Microsoft::WRL::ComPtr;
 
 #define SURFACE_NUM 1
 #define MIN(a, b) ((a)>(b)?(b):(a))
@@ -35,25 +40,67 @@ private:
     void onInputData(uint32_t pipeId, const uint8_t *data, uint32_t len, bool complete);
 
     static const UINT FrameCount = 2;
-//    ComPtr<ID3D12Device> device;
-//    ComPtr<ID3D12Resource> renderTargets[FrameCount];
 
+    //pipeline objects
+    ComPtr<IDXGISwapChain3> swapChain;
+    ComPtr<IDXGIAdapter1> adapter;
+    ComPtr<ID3D12Device> device;
+    ComPtr<ID3D12Resource> renderTargets[FrameCount];
+    ComPtr<ID3D12CommandAllocator> commandAllocator;
+    ComPtr<ID3D12CommandQueue> commandQueue;
+    ComPtr<ID3D12DescriptorHeap> rtvHeap;
+    ComPtr<ID3D12PipelineState> pipelineState;
+    ComPtr<ID3D12GraphicsCommandList> commandList;
+    UINT m_rtvDescriptorSize = 0;
+
+    //va objects
     VADisplay vaDisplay;
+    VASurfaceID vaRenderTargets[FrameCount] = { };
     static const uint32_t vaNumRGBASurfaces = 16;
     VASurfaceID vaRGBASurfaces[vaNumRGBASurfaces] = { };
     VASurfaceID vaSurfaceNV12 = 0;
+    UINT numVPRegions;
+    VAConfigID vaProcConfigId = 0;
+    // Context for color rgb to yuv conversion
+    VAContextID vaColorConvCtx = 0;
+    VABufferID vaColorConvBuf = 0;
+    VAContextID vaCopyCtx = 0;
+    VABufferID vaCopyBuf = 0;
+    VAContextID vaBlendCtx = 0;
+    VABufferID vaBlendBuf = 0;
+    VAProcPipelineCaps procPipelineCaps = { };
+    const float alphaBlend = 0.75f;
+    const float regionsSizeRatio = 1.2f;
+    static const UINT regionVariations = 216;
+    UINT curRegionVariation = 0;
+    VARectangle pBlendRegions[regionVariations/*Prepare two sets of regions so there's some motion*/][vaNumRGBASurfaces];
+    float colors[regionVariations][4];
 
-    bool initVaEncoder();
+    // Video Encoder
+    VAConfigID vaEncConfigId = 0;
+    VAContextID vaEncContextId = 0;
+    VABufferID vaEncPipelineBufferId[4];
+    static const VABufferID VA_H264ENC_BUFFER_INDEX_SEQ = 0;
+    static const VABufferID VA_H264ENC_BUFFER_INDEX_PIC = 1;
+    static const VABufferID VA_H264ENC_BUFFER_INDEX_SLICE = 2;
+    static const VABufferID VA_H264ENC_BUFFER_INDEX_COMPRESSED_BIT = 3;
+    static const UINT H264_MB_PIXEL_SIZE = 16;
+    std::ofstream finalEncodedBitstream;
 
     static void vaInfoCallback(void *context, char *message);
     static void vaErrorCallback(void *context, char *message);
 
     bool initVaPipeline();
+    bool initD3D12Pipeline();
+
     bool initVaDisplay();
     bool ensureVaProcSupport();
     bool ensureVaEncSupport();
     bool createVaSurfaces();
     bool importRenderTargetsToVa();
+    bool initVaProcContext();
+    bool initVaEncoder();
+    bool initVaEncContext();
 };
 
 #endif //SNPSERVER_SNPENCODERVAH264_H
